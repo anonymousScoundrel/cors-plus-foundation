8f6e4d49ad33d1effb8f5f751b896f14ea4abe3a
Theo Coukoulis <root@addmorehacks.com>
2015-08-15 00:13:11 -0500

    Tie cors request to the button click

--- js/app.js
+++ js/app.js
@@ -2,24 +2,32 @@
   "use strict";
 
   $(document).ready(function () {
-    var xhr = new XMLHttpRequest();
-    var url = 'http://www.addmorehacks.com/cors/index.php';
-    var handler = function () {
-      console.log(xhr.responseText);
-    };
 
     // Next steps:
-    // tie the cors request to button click
     // swap out images
     // test
 
-    if (xhr) {
-      xhr.open('POST', url2, true);
-      xhr.setRequestHeader('X-Fetch-Images', 3);
-      xhr.onerror = handler;
-      xhr.onload = handler;
-      xhr.send();
-    }
+    document.getElementById("cors-button").addEventListener(
+      'click',
+      function (event) {
+        var button = this;
+        var xhr = new XMLHttpRequest();
+        var url = 'http://www.addmorehacks.com/cors/index.php';
+        var handler = function () {
+          console.log(xhr.responseText);
+        };
+
+        if (xhr) {
+          xhr.open('POST', url, true);
+          xhr.setRequestHeader('X-Fetch-Images', 3);
+          xhr.onerror = handler;
+          xhr.onload = handler;
+          xhr.send();
+        }
+      },
+      true
+    );
+
     $(document).foundation();
   });
 }(jQuery));
